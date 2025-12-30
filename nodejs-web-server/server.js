9faf3b6c9ae609b8c67ac8391ca7f7e40e193820
andy.kristanto <andy.kristanto@IT20>
2025-12-30 16:00:13 +0700

    refactor: implementation routing based on url and http methods

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { isGeneratorFunction } = require('util/types');
 
 /*
 // Previous simple response example
@@ -10,6 +11,7 @@ const requestListener = (request, response) => {
 }
 */
 
+/*
 // Implementing different responses based on HTTP methods
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
@@ -33,6 +35,83 @@ const requestListener = (request, response) => {
     response.end('<h1>Salam!</h1>');
   }
 };
+*/
+
+/*
+// Handling POST request data
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+  response.statusCode = 200;
+
+  const { method } = request;
+
+  if (method === 'GET') {
+    response.end('<h1>Hello!</h1>');
+  }
+
+  if (method === 'POST') {
+    let body = [];
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      body = Buffer.concat(body).toString();
+      const name = JSON.parse(body).name;
+      response.end(`<h1>Hai, ${name}!</h1>`);
+    });
+  }
+};
+
+// output: <h1>Hai, Dimas!</h1>
+*/
+
+/*
+
+// Routing based on URL and HTTP methods
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+  response.statusCode = 200;
+
+  const { method, url } = request;
+
+  if(url === '/'){
+    if(method === 'GET') {
+      response.end('<h1>Ini adalah halaman utama</h1>');
+    }else{
+      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+    }
+
+  } 
+  else if(url === '/about') 
+  {
+    if(method === 'GET') 
+    {
+      response.end('<h1>Halo! Ini adalah halaman about</h1>');
+    } 
+    else if(method === 'POST') 
+    {
+      let body = [];
+
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
+  
+      request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        const name = JSON.parse(body).name;
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
+    } 
+    else 
+    {
+      response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+    }
+  }
+};
+*/
 
 const server = http.createServer(requestListener);
 
