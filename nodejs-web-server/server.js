9f0ffd6a374d28e4ca7802530a6cb672be928f4b
andy.kristanto <andy.kristanto@IT20>
2025-12-30 16:24:04 +0700

    refactor: add proper status codes in response

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { resolve } = require('path');
 const { isGeneratorFunction } = require('util/types');
 
 /*
@@ -109,6 +110,59 @@ const requestListener = (request, response) => {
     {
       response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
     }
+  }
+};
+*/
+
+/*
+// Version with proper status codes
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+
+  const { method, url } = request;
+
+  if(url === '/'){
+    if(method === 'GET') {
+      response.statusCode = 200;
+      response.end('<h1>Ini adalah halaman utama</h1>');
+    }else{
+      response.statusCode = 400;
+      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+    }
+
+  } 
+  else if(url === '/about') 
+  {
+    if(method === 'GET') 
+    {
+      response.statusCode = 200;
+      response.end('<h1>Halo! Ini adalah halaman about</h1>');
+    } 
+    else if(method === 'POST') 
+    {
+      let body = [];
+
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
+  
+      request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        const name = JSON.parse(body).name;
+        response.statusCode = 200;
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
+    } 
+    else 
+    {
+      response.statusCode = 400;
+      response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+    }
+  }
+  else
+  {
+    response.statusCode = 404;
+    response.end('<h1>Halaman tidak ditemukan!</h1>');
   }
 };
 */
